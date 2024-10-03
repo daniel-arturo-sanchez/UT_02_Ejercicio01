e072fa19d247565040140e9804406e31b9727ad0
2dawa <2dawa@informatica.edu>
2024-10-03 09:53:59 +0100

    Modelo (casi) listo

---
+++ UT_02_Ejercicio_01/Models/Car.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Server.Kestrel.Core;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace UT_02_Ejercicio_01.Models
+{
+    public class Car
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(20,ErrorMessage = "Este campo debe contener entre 3 y 20 caracteres.", MinimumLength = 3)]
+        [DisplayName("Modelo")]
+        public string Model { get; set; }
+
+        [Required]
+        [StringLength(15, ErrorMessage = "Este campo debe contener entre 3 y 15 caracteres.", MinimumLength = 3)]
+        [DisplayName("Marca")]
+        public string Brand { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayName("Fecha de lanzamiento")]
+        public DateTime ReleaseDate { get; set; }
+
+        [Required]
+        [Range(5000,30000,ErrorMessage = "El precio del coche debe estar comprendido entre 5000 y 30000")]
+        [DisplayName("Precio")]
+        public decimal Price { get; set; }
+    }
+}
