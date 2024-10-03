dff7dafa2b3a0e9921cdee7ab7799c5e92438156
2dawa <2dawa@informatica.edu>
2024-10-03 09:17:04 +0100

    Create project ASP.NET Core MVC

---
+++ UT_02_Ejercicio_01/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using UT_02_Ejercicio_01.Models;
+
+namespace UT_02_Ejercicio_01.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ UT_02_Ejercicio_01/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UT_02_Ejercicio_01.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
